0d9dd2e3d9f4d95efc0c49a328f4c37a5cc69c06
joelk <joelkukin@gmail.com>
2025-09-22 18:33:59 -0300

    conexion DB adaptable a docker y xampp

--- db/Database.php
+++ db/Database.php
@@ -4,11 +4,10 @@ class Database {
     private $conn;
 
     public function __construct() {
-        $host = getenv('DB_HOST');
-        $dbname = getenv('DB_NAME');
-        $user = getenv('DB_USER');
-        $pass = getenv('DB_PASS');
-
+        $host = getenv('DB_HOST') ? getenv('DB_HOST'): "localhost";
+        $dbname = getenv('DB_NAME') ? getenv('DB_NAME'): "TP-Sistemas-nro1";
+        $user = getenv('DB_USER') ? getenv('DB_USER'): "root";
+        $pass = getenv('DB_PASS') ? getenv('DB_PASS'): "";
         $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
         $this->conn = new PDO($dsn, $user, $pass);
     }
