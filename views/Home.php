6d27d46085502a7a91b5d1e1ac94cf36057d7fa6
Joel Kukin <joelkukin@gmail.com>
2025-10-19 19:56:10 -0300

    fixed home

--- views/Home.php
+++ views/Home.php
@@ -1,9 +1,7 @@
 
-<?php 
-function Home () {
-?>
 
 
+<!-- 
 <!DOCTYPE html>
 <html lang="es">
 
@@ -11,16 +9,14 @@ function Home () {
 
   <h1 class="text-2xl font-bold mb-4">Partidos</h1>
   <div id="contenedor-partidos"></div>
-
-  <!-- Script de la tarjeta -->
-  <script src="public/js/CrearTarjeta.js"></script>
-  <script src="public/js/index2.js"></script>
   
-</body>
-</html>
-<?php
-}?>
-<?php
+  <!-- Script de la tarjeta -->
+  <!-- <script src="public/js/CrearTarjeta.js"></script>
+  
+  </body>
+  </html> -->
+   
+<?php 
 function Home() { 
 ?>
     <h1 class="text-2xl font-bold my-4 text-center">Bienvenido <?php echo $_SESSION["role"]." ". $_SESSION["name"];?></h1>
